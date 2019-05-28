4cb73ad343a2acc199e409dae80f5e0032907bbd
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2019-05-28 22:26:18 +0900

    Add files via upload

--- p1.py
+++ p1.py
@@ -4,8 +4,10 @@ import random
 
 def hjp(n):
     for i in range(n):        
+        
+        background_num = random.randrange(3) # write your background files count in parameter
 
-        img1 = cv2.imread('images/me2.jpg')
+        img1 = cv2.imread('background/bg%d.jpg' % (background_num))
         img2 = cv2.imread('images/cross.png')
 
         rows1, cols1, channels1 = img1.shape
@@ -14,7 +16,7 @@ def hjp(n):
         #print(rows1, cols1, channels1)
         #print(rows2, cols2, channels2)
 
-        y = random.randrange(100, 500)
+        y = random.randrange(200, 800)
         x = (y * 2 )// 3
 
         vpos = random.randrange(0, rows1 - y)
@@ -29,7 +31,7 @@ def hjp(n):
         
         
         roi = img1[vpos:vpos+y, hpos:hpos+x]
-
+        
         img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
         ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
         mask_inv = cv2.bitwise_not(mask)
@@ -51,7 +53,8 @@ def hjp(n):
         #hpos: 오른쪽 위
         #hpos + x: 오른쪽 아래
         f = open("save/test%d.txt" % (i), 'w')
-        f.write('%d %d %d %d' % (vpos, vpos + y, hpos + x, hpos))
+        f.write('%d %.6f %.6f %.6f %.6f' % (0, (hpos + x/2) / rows1, (vpos + y/2) / cols1, x / rows1, y / cols1))
+        # 클래스번호, 왼쪽으로부터 사진 중심, 위쪽으로부터 사진 중심, 사진 가로, 사진 세로 (비율)
         f.close()
         #cv2.imshow('result', img1)
         #cv2.waitKey(0)
